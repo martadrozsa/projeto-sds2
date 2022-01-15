f029dcdad3f19604086605fb814d01ea3b3a914a
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2022-01-15 20:23:47 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/backend/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/backend/controllers/OrderController.java
@@ -4,10 +4,10 @@ import com.devsuperior.backend.dto.OrderDTO;
 import com.devsuperior.backend.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,5 +25,12 @@ public class OrderController {
     public ResponseEntity<List<OrderDTO>> findAll() {
         List<OrderDTO> list = orderService.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO orderDTO) {
+        orderDTO = orderService.insert(orderDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(orderDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(orderDTO);
     }
 }
--- backend/src/main/java/com/devsuperior/backend/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/backend/services/OrderService.java
@@ -1,12 +1,17 @@
 package com.devsuperior.backend.services;
 
 import com.devsuperior.backend.dto.OrderDTO;
+import com.devsuperior.backend.dto.ProductDTO;
 import com.devsuperior.backend.entities.Order;
+import com.devsuperior.backend.entities.Product;
+import com.devsuperior.backend.entities.enums.OrderStatus;
 import com.devsuperior.backend.repositories.OrderRepository;
+import com.devsuperior.backend.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,15 +20,31 @@ public class OrderService {
 
 
     private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
 
     @Autowired
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, ProductRepository productRepository) {
         this.orderRepository = orderRepository;
+        this.productRepository = productRepository;
     }
 
     @Transactional(readOnly = true)
     public List<OrderDTO> findAll() {
         List<Order> list = orderRepository.findOrdersWithProducts();
         return list.stream().map(OrderDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO orderDTO) {
+
+        Order order = new Order(null, orderDTO.getAddress(), orderDTO.getLatitude(), orderDTO.getLongitude(), Instant.now(), OrderStatus.PENDING);
+
+        for (ProductDTO p : orderDTO.getProductDTOList()) {
+            Product product = productRepository.getOne(p.getId());
+            order.getProducts().add(product);
+        }
+
+        order = orderRepository.save(order);
+        return new OrderDTO(order);
     }
 }
